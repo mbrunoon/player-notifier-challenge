efe0f230516b623eabfcfc2ba39294d4e21c5df9
Marcos Bruno <marcos.bruno@hurr.com>
2022-10-25 14:28:54 -0300

    Create Player Scaffold

---
+++ db/migrate/20221025171743_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :number
+      t.string :nationality
+      t.date :birthdate
+      t.string :position
+
+      t.timestamps
+    end
+  end
+end
