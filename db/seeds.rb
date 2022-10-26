9dd9afa30844f857c1d30eb3d4a1ba32def9c55c
Marcos Bruno <marcos.bruno@hurr.com>
2022-10-26 09:01:32 -0300

    Create db seed for teams and players

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'net/http'
+require 'json'
+
+
+teams_and_players = Net::HTTP.get(URI("https://raw.githubusercontent.com/RealFevr/challenge/master/data/players.json"))    
+teams_and_players = JSON.parse(teams_and_players)
+teams_and_players = teams_and_players["data"]["teams"]
+
+teams_and_players.each do |team|
+    puts "Inserting '#{team["name"]}' and players."
+    Team.create({
+        name: team["name"],
+        acronym: team["acronym"],
+        players_attributes: team["players"]
+    })
+end
