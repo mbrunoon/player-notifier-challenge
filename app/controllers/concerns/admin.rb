6c3b67b0e733f6ea04d7289f0cb4bd2a46cba2f3
Marcos Bruno <marcos.bruno@hurr.com>
2022-11-02 11:21:48 -0300

    Update Role validation

--- app/controllers/concerns/admin.rb
+++ app/controllers/concerns/admin.rb
@@ -3,7 +3,18 @@ module Admin
 
     included do        
         before_action :authenticate_user!
-        layout 'admin'        
+        before_action :check_user_role
+        layout 'admin'                   
+    end
+
+
+    def check_user_role                
+        if current_user.role != "admin"    
+            respond_to do |format|
+                format.html{ redirect_to root_page, status: unauthorized, notice: "unauthorized action" }
+                format.json { render json: {}, status: :unauthorized }
+            end            
+        end
     end
 
 end
--- app/controllers/concerns/supporter.rb
+++ app/controllers/concerns/supporter.rb
@@ -3,7 +3,17 @@ module Supporter
 
     included do
         before_action :authenticate_user!
+        before_action :check_user_role
         layout 'supporter'
     end    
+
+    def check_user_role                
+        if current_user.role != "supporter"    
+            respond_to do |format|
+                format.html{ redirect_to root_page, status: unauthorized, notice: "unauthorized action" }
+                format.json { render json: {}, status: :unauthorized }
+            end            
+        end
+    end
 
 end
